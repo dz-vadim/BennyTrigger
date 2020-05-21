4f22266d83d327910754bbb6848d38249ba581a0
Student <student@corp.ies.org.ua>
2020-05-22 01:27:47 +0300

    Revert "add jump"
    This reverts commit f4c250816418a2944d032d92dd9c2a034359bd9f.

--- BennyTrigger/Assets/Scripts/StartAnimationInTrigger.cs
+++ BennyTrigger/Assets/Scripts/StartAnimationInTrigger.cs
@@ -4,19 +4,10 @@ using UnityEngine;
 
 public class StartAnimationInTrigger : MonoBehaviour
 {
-    public float moveSpeed;    
-    private float speedFactor=0.1f;
-
-    private void FixedUpdate()
-    {
-        transform.position += transform.forward * moveSpeed*speedFactor;
-    }
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.tag == "StartJump")
         {
-            //gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
-            moveSpeed = 0;
             gameObject.GetComponent<Animator>().SetBool("Jump", true);
         }        
     }
