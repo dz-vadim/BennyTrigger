5cf8d74497c84f1b06369e80e19ee79e1100d00d
Student <student@corp.ies.org.ua>
2020-05-22 02:00:57 +0300

    Add corect animation run and jump

--- BennyTrigger/Assets/Scripts/StartAnimationInTrigger.cs
+++ BennyTrigger/Assets/Scripts/StartAnimationInTrigger.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class StartAnimationInTrigger : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider collider)
+    public float moveSpeed;
+    private float speedFactor=0.1f;
+    void FixedUpdate()
+    {
+        transform.position += transform.forward * moveSpeed*speedFactor;
+    }
+        private void OnTriggerEnter(Collider collider)
     {
         if (collider.tag == "StartJump")
         {
             gameObject.GetComponent<Animator>().SetBool("Jump", true);
+            speedFactor = 0;
         }        
     }
 }
