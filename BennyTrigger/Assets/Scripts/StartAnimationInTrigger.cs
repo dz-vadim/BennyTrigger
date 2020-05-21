edc515ba7e8aef0e8699cc63cf8e41e9534cb4df
Student <student@corp.ies.org.ua>
2020-05-22 02:09:17 +0300

    Revert "Add corect animation run and jump"
    This reverts commit 5cf8d74497c84f1b06369e80e19ee79e1100d00d.

--- BennyTrigger/Assets/Scripts/StartAnimationInTrigger.cs
+++ BennyTrigger/Assets/Scripts/StartAnimationInTrigger.cs
@@ -4,18 +4,11 @@ using UnityEngine;
 
 public class StartAnimationInTrigger : MonoBehaviour
 {
-    public float moveSpeed;
-    private float speedFactor=0.1f;
-    void FixedUpdate()
-    {
-        transform.position += transform.forward * moveSpeed*speedFactor;
-    }
-        private void OnTriggerEnter(Collider collider)
+    private void OnTriggerEnter(Collider collider)
     {
         if (collider.tag == "StartJump")
         {
             gameObject.GetComponent<Animator>().SetBool("Jump", true);
-            speedFactor = 0;
         }        
     }
 }
